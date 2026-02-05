ed108c1e83d7221310f065549807e0cc4f3330c3
pbobell <pbobell@users.noreply.github.com>
2026-02-04 19:25:09 -0800

    Finished textnode.py with tests

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from textnode import TextNode, TextType
 
 def main():
-    test = TextNode("This is some anchor text", TextType.LINK_TEXT, "https://www.boot.dev")
+    test = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
     print(test.__repr__())
 
 

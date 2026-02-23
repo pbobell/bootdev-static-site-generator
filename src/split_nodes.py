96ccca9d5ebb5c65f1f69f9700c717bd0db13c6f
pbobell <pbobell@users.noreply.github.com>
2026-02-22 20:05:50 -0800

    I seem to have forgotten to save split_nodes.py, now I have.

--- src/split_nodes.py
+++ src/split_nodes.py
@@ -28,5 +28,24 @@ def split_nodes_image(old_nodes):
 
 def split_nodes_link(old_nodes):
     new_nodes = []
-    pass
-
+        
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        link_info = extract_markdown_links(node.text)
+        if not link_info:
+            new_nodes.append(node)
+            continue
+        original_text = node.text
+        for i in range(len(link_info)):
+            current_text = original_text.split(f"[{link_info[i][0]}]({link_info[i][1]})", 1)
+            if len(current_text) != 2:
+                raise ValueError(f"Failed to split node around link markdown")
+            if current_text[0] != "":
+                new_nodes.append(TextNode(current_text[0], TextType.TEXT))
+            new_nodes.append(TextNode(link_info[i][0], TextType.LINK, link_info[i][1]))
+            original_text = current_text[1]
+        if original_text != "":
+            new_nodes.append(TextNode(original_text, TextType.TEXT))
+    return new_nodes
